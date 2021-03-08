35f31274b731ae7be81deda20991e07a9fcab87c
pranavshukla1011 <shuklapranav1011@gmail.com>
2021-03-08 13:55:17 +0530

    completed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function getJokes(e){
 
     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET',`https://api.icndb.com/jokes/random/${num}`, true);
+    xhr.open('GET',`http://api.icndb.com/jokes/random/${num}`, true);
 
     xhr.onload = function(){
         
